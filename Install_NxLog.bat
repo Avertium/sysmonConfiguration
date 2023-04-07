e36f2bf8943f6e505f9227904498b058a9207847
C. Crank <69054619+Crankinator@users.noreply.github.com>
2023-04-07 09:50:37 -0400

    adding nxlog install script

---
+++ Install_NxLog.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+sc query "nxlog" | Find "RUNNING" >NUL
+echo [+] Downloading NxLog Community Edition...
+@powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/TerraVerde/sysmonConfiguration/raw/master/nxlog-ce-3.1.2319.msi','nxlog-ce.3.1.2319.msi')	
+echo [+] Installing NxLog Community Edition...
+nxlog-ce-3.1.2319.msi /quiet
+sc failure nxlog actions= restart/10000/restart/10000// reset= 120
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/TerreVerde/sysmonConfiguration/master/nxlog.conf','C:\Program Files (x86)\nxlog\conf\nxlog.conf')"
+echo [+] Starting NxLog Community Edition...
+net start nxlog
+:exit
